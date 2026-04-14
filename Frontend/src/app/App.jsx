ac01f4d4ad44d2210aa801aedad76d1a21a1fe14
piyush <piyushujgaokar544@gmail.com>
2026-04-14 10:56:33 +0530

    integrate react-router and implement authentication logic

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
 import './App.css'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
-const App = () => {
+function App() {
+
   return (
-    <div>Hello World</div>
+    <>
+      <RouterProvider router={routes} />
+    </>
   )
 }
 
---
+++ Frontend/src/features/auth/hook/useAuth.js
@@ -0,0 +1,19 @@
+import { setError, setLoading, setUser } from "../state/auth.slice"
+import { register } from "../services/auth.api"
+import { useDispatch } from "react-redux"
+
+
+
+export const useAuth = () => {
+
+    const dispatch = useDispatch()
+
+    async function handleRegister({ email, contact, password, fullname, isSeller = false }) {
+
+        const data = await register({ email, contact, password, fullname, isSeller })
+
+        dispatch(setUser(data.user))
+    }
+
+
+}
